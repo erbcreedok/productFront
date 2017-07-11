71f6cdcc4b9bf2f505087f8204168c15789f12b7
Asanali Isataev <Assanali@MacBook-Pro-Asanali.local>
2017-07-11 16:23:54 +0300

    Revert "Get All service Function. Api Connect"
    This reverts commit 13cf5faf9e6f4a9dc6e1203fac4d3a6cc7e01996.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
-import {HttpModule} from '@angular/http';
-import {ServerService} from './shared/server.service';
-import { HeaderComponent } from './header/header.component';
-import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent,
-    ProductDetailComponent,
-    HeaderComponent,
-    ProductsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpModule,
+    BrowserModule
   ],
-  providers: [ServerService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/products/product.model.ts
+++
@@ -1,4 +0,0 @@
-export class Product {
-    constructor(public productCode: string, public productName: string, public productDescription: string,
-                public stock: number, public cost: number, public discontinued: Date, public dateAdded: Date) {}
-}
