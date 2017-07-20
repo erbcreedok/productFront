3c67e6d33fe78dde94ec170e57c03c0ceb94d19a
erbcreedok <admin@MacBookPro.local>
2017-07-20 21:18:44 +0600

    Added Property Names to Product Model

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -3,7 +3,12 @@ import {ProductService} from '../product.service';
 import {Product} from '../product.model';
 import {Subscription} from "rxjs/Subscription";
 
-
+interface ProductColumn {
+    name: string,
+    title: string,
+    type: string,
+    property: string
+}
 
 @Component({
   selector: 'app-product-list',
@@ -12,7 +17,7 @@ import {Subscription} from "rxjs/Subscription";
 })
 export class ProductListComponent implements OnInit, OnDestroy {
   products: Product[];
-  productColumns: {name: string, title: string}[];
+  productColumns: ProductColumn[];
   productSubscription: Subscription;
 
   isFilterOpen = false;
@@ -34,15 +39,15 @@ export class ProductListComponent implements OnInit, OnDestroy {
       this.productSubscription.unsubscribe();
   }
 
-  isOrderedBy(columnName: string) {
-    return this.productService.order.sort === columnName ? 'active' + (this.productService.order.order ? ' ' : ' reverse') : '';
+  isOrderedBy(columnName: ProductColumn) {
+    return this.productService.order.sort === columnName.name ? 'active' + (this.productService.order.order ? ' ' : ' reverse') : '';
   }
 
-  onSorted(columnName: string) {
-    if (this.productService.order.sort === columnName) {
+  onSorted(columnName: ProductColumn) {
+    if (this.productService.order.sort === columnName.name) {
       this.productService.order.order = !this.productService.order.order;
     } else {
-      this.productService.order.sort = columnName;
+      this.productService.order.sort = columnName.property;
       this.productService.order.order = false;
     }
     this.productService.getProductsByOptions();
